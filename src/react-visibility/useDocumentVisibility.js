d9e2a101f7a39b65e25ccd6992137d3f1c5afe83
rpopov <rpopov@web-bee.ru>
2022-08-07 10:39:35 +0300

    react-visibility template

---
+++ src/react-visibility/useDocumentVisibility.js
@@ -0,0 +1,42 @@
+import { useState,useEffect, useRef } from 'react';
+
+function getDocumentVisibility() {
+    if (typeof document === "undefined") {
+        return true;
+    }
+    return !document.hidden;
+}
+
+const useDocumentVisibility = () => {
+    const [visible, setIsVisible] = useState(getDocumentVisibility());
+    const [count, setcount] = useState(0);
+    const callbacks = useRef([]);
+
+    const onVisibilityChangeAll = () => {
+        if(getDocumentVisibility() === false){
+            setcount(count => count + 1);
+        }
+        setIsVisible(getDocumentVisibility());
+        callbacks.current.forEach(element => {
+            element();
+        });
+    };
+
+    const onVisibilityChange = (callback) => {
+        const event = (() => callback(getDocumentVisibility()));
+        callbacks.current.push(() => event());
+    };
+
+    useEffect(() => {
+        document.addEventListener('visibilitychange', onVisibilityChangeAll, false);
+        const cleanCallbacks = callbacks.current;
+        return () => {
+            document.removeEventListener('visibilitychange', onVisibilityChangeAll);
+            cleanCallbacks.length = 0;
+        };
+    }, []);
+
+    return { count,  visible, onVisibilityChange};
+};
+
+export default useDocumentVisibility;
